c19a7db35d3ac1d0859e000087f94c4fc95e639b
anirudh <anirudhece2020@gmail.com>
2022-04-26 19:33:05 +0530

    js update

---
+++ index.js
@@ -0,0 +1,27 @@
+let calcScreen= document.getElementById('input');
+let button= document.querySelectorAll('button');
+
+let input= ''; //string which will show the operation on screen
+
+for(let item of button){
+    item.addEventListener('click', (e)=>{
+        let text= e.target.innerText ;
+        console.log(text);
+        
+        if(text=='C'){
+            calcScreen.value= '';
+            input= calcScreen.value;
+        }
+        
+        else if(text=='='){
+            calcScreen.value=eval(calcScreen.value);
+        }
+        
+        else{
+            input += text;
+            calcScreen.value=input;
+        }
+
+    });
+    
+}
